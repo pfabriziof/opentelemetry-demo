b4d131d68a197fb2e449a55d5400fd605dd6b44f
Paolo <paolofalcon98@gmail.com>
2026-07-03 12:00:18 -0500

    fix(zipkin): update ZipkinExporter parameters to post traces inside the container network

--- dice_roller/instrumentation.ts
+++ dice_roller/instrumentation.ts
@@ -2,12 +2,19 @@ import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new ZipkinExporter(),
-  instrumentations: [getNodeAutoInstrumentations()],
-  metricReader: new PrometheusExporter({
-      port: 9464,
-  }),
+    resource: resourceFromAttributes({
+        [ATTR_SERVICE_NAME]: 'dice-server',
+    }),
+    traceExporter: new ZipkinExporter({
+        url: 'http://zipkin:9411/api/v2/spans',
+    }),
+    metricReader: new PrometheusExporter({
+        port: 9464,
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
 });
 sdk.start();
